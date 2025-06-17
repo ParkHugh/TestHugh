c2b1beb42b6e20f956f379b3fa9f2e88b83ebd2a
박현규 <hughpark@baghyeongyuui-MacBookPro.local>
2025-06-17 22:03:57 +0900

    fix: client 내부 변경사항 커밋

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import TetoTest from './pages/TetoTest'; // PersonalityTest에서 파일명만 변경!
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import Contact from './pages/Contact';
+import './index.css'; // CSS 그대로 사용
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/tetotest" element={<TetoTest />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* 앞으로 다른 테스트도 아래에 계속 추가하면 됨 */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
