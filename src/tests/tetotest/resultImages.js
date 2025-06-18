cf1e223d5db9c9be0b11a6369b88adf13f72e2f2
박현규 <hughpark@baghyeongyuui-MacBookPro.local>
2025-06-18 23:55:52 +0900

    fix all english

--- src/tests/tetotest/resultImages.js
+++ src/tests/tetotest/resultImages.js
@@ -1,18 +1,18 @@
 // src/resultImages.js
-import 테토남 from './resultImages/테토남.png';
-import 테토녀 from './resultImages/테토녀.png';
-import 에겐남 from './resultImages/에겐남.png';
-import 에겐녀 from './resultImages/에겐녀.png';
-import 테겐남 from './resultImages/테겐남.png';
-import 테겐녀 from './resultImages/테겐녀.png';
+import tetonam from './Images/tboy.png';
+import tetogirl from './Images/tgirl.png';
+import egenboy from './Images/etboy.png';
+import egengirl from './Images/etgirl.png';
+import tegenboy from './Images/eboy.png';
+import tegengirl from './Images/egirl.png';
 
 const resultImages = {
-  테토남,
-  테토녀,
-  에겐남,
-  에겐녀,
-  테겐남,
-  테겐녀,
+  tetonam,
+  tetogirl,
+  egenboy,
+  egengirl,
+  tegenboy,
+  tegengirl
 };
 
 export default resultImages;
