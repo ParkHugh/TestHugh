703c8b9841556e31a077de09e2ad39f53c7c83ea
박현규 <hughpark@baghyeongyuui-MacBookPro.local>
2025-06-18 22:54:16 +0900

    코드 재정리/메인수정

---
+++ src/tests/tetotest/meta.js
@@ -0,0 +1,10 @@
+// src/tests/tetotest/meta.js
+import mainImage from './images/main.png';
+
+export default {
+  id: 'tetotest',
+  title: '테토/에겐/테겐 테스트',
+  description: '테토녀·에겐남? 나의 호르몬 성향은 어디에 가까울까?',
+  image: mainImage,
+  path: '/tetotest', // 혹시 경로가 다를 경우 명확하게
+};
