7923367785a3a31907cfea082724c1e8b64f6b4c
박현규 <hughpark@baghyeongyuui-MacBookPro.local>
2025-06-18 23:23:01 +0900

    fix: 이미지 경로/파일명 대소문자 수정

--- src/tests/tetotest/resultImages.js
+++ src/tests/tetotest/resultImages.js
@@ -1,10 +1,10 @@
 // src/resultImages.js
-import 테토남 from './img/테토남.png';
-import 테토녀 from './img/테토녀.png';
-import 에겐남 from './img/에겐남.png';
-import 에겐녀 from './img/에겐녀.png';
-import 테겐남 from './img/테겐남.png';
-import 테겐녀 from './img/테겐녀.png';
+import 테토남 from './images/테토남.png';
+import 테토녀 from './images/테토녀.png';
+import 에겐남 from './images/에겐남.png';
+import 에겐녀 from './images/에겐녀.png';
+import 테겐남 from './images/테겐남.png';
+import 테겐녀 from './images/테겐녀.png';
 
 const resultImages = {
   테토남,
