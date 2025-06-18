6cd3025868501b0eda34293bc7962bc6f095e3e9
박현규 <hughpark@baghyeongyuui-MacBookPro.local>
2025-06-18 23:38:40 +0900

    한글까지 다 제거 main.png만남김

--- src/tests/tetotest/resultImages.js
+++ src/tests/tetotest/resultImages.js
@@ -1,10 +1,10 @@
 // src/resultImages.js
-import 테토남 from './images/테토남.png';
-import 테토녀 from './images/테토녀.png';
-import 에겐남 from './images/에겐남.png';
-import 에겐녀 from './images/에겐녀.png';
-import 테겐남 from './images/테겐남.png';
-import 테겐녀 from './images/테겐녀.png';
+import 테토남 from './resultImages/테토남.png';
+import 테토녀 from './resultImages/테토녀.png';
+import 에겐남 from './resultImages/에겐남.png';
+import 에겐녀 from './resultImages/에겐녀.png';
+import 테겐남 from './resultImages/테겐남.png';
+import 테겐녀 from './resultImages/테겐녀.png';
 
 const resultImages = {
   테토남,
