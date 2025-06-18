c05339356a8dcabd0f388feb56f649fc7955b78e
박현규 <hughpark@baghyeongyuui-MacBookPro.local>
2025-06-19 00:18:45 +0900

    폰트 추가 후 반영

--- src/tests/tetotest/resultImages.js
+++ src/tests/tetotest/resultImages.js
@@ -1,18 +1,18 @@
 // src/resultImages.js
-import tetonam from './images/tboy.png';
-import tetogirl from './images/tgirl.png';
-import egenboy from './images/etboy.png';
-import egengirl from './images/etgirl.png';
-import tegenboy from './images/eboy.png';
-import tegengirl from './images/egirl.png';
+import 테토남 from './images/tboy.png';
+import 테토녀 from './images/tgirl.png';
+import 에겐남 from './images/etboy.png';
+import 에겐녀 from './images/etgirl.png';
+import 테겐남 from './images/eboy.png';
+import 테겐녀 from './images/egirl.png';
 
 const resultImages = {
-  tetonam,
-  tetogirl,
-  egenboy,
-  egengirl,
-  tegenboy,
-  tegengirl
+  테토남,
+  테토녀,
+  에겐남,
+  에겐녀,
+  테겐남,
+  테겐녀
 };
 
 export default resultImages;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'brand': ['Playfair Display', 'Noto Serif KR', 'serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
