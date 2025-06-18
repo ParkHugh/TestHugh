324e8c7a16ddab5a2e66778f8ab601245570e9c4
박현규 <hughpark@baghyeongyuui-MacBookPro.local>
2025-06-19 00:02:06 +0900

    fix: 대소문자 수정

--- src/tests/tetotest/resultImages.js
+++ src/tests/tetotest/resultImages.js
@@ -1,10 +1,10 @@
 // src/resultImages.js
-import tetonam from './Images/tboy.png';
-import tetogirl from './Images/tgirl.png';
-import egenboy from './Images/etboy.png';
-import egengirl from './Images/etgirl.png';
-import tegenboy from './Images/eboy.png';
-import tegengirl from './Images/egirl.png';
+import tetonam from './images/tboy.png';
+import tetogirl from './images/tgirl.png';
+import egenboy from './images/etboy.png';
+import egengirl from './images/etgirl.png';
+import tegenboy from './images/eboy.png';
+import tegengirl from './images/egirl.png';
 
 const resultImages = {
   tetonam,
