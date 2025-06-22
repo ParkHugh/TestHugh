e73fbef61407e0c67b7bb5adb40d8862409d8e7e
박현규 <hughpark@baghyeongyuui-MacBookPro.local>
2025-06-23 01:26:25 +0900

    일단 최종

--- src/tests/tetotest/resultImages.js
+++ src/tests/tetotest/resultImages.js
@@ -1,18 +1,18 @@
 // src/resultImages.js
 import 테토남 from './images/tboy.png';
 import 테토녀 from './images/tgirl.png';
-import 에겐남 from './images/etboy.png';
-import 에겐녀 from './images/etgirl.png';
-import 테겐남 from './images/eboy.png';
-import 테겐녀 from './images/egirl.png';
+import 테겐남 from './images/etboy.png';
+import 테겐녀 from './images/etgirl.png';
+import 에겐남 from './images/eboy.png';
+import 에겐녀 from './images/egirl.png';
 
 const resultImages = {
   테토남,
   테토녀,
+  테겐남,
+  테겐녀,
   에겐남,
   에겐녀,
-  테겐남,
-  테겐녀
 };
 
 export default resultImages;
