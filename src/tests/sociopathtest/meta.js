c71f85ec394cf0c7200e31e1de6bcad1edbbb864
박현규 <hughpark@baghyeongyuui-MacBookPro.local>
2025-07-02 23:12:12 +0900

    sociopath ok

--- src/tests/sociopathtest/meta.js
+++ src/tests/sociopathtest/meta.js
@@ -4,6 +4,6 @@ const meta = {
   title: '직장인 소시오패스 테스트',
   description: '나는 직장에서 천사일까, 은근한 소시오패스일까?',
   image: '/images/sociopathtest/main.png',
-  path: '/sociopath', // 혹시 경로가 다를 경우 명확하게
+  path: '/sociopathtest', // 혹시 경로가 다를 경우 명확하게
 };
 export default meta;
