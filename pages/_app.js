29a1d13c59b39bf00788db7f80aa1093129119f9
박현규 <hughpark@baghyeongyuui-MacBookPro.local>
2025-07-04 09:47:05 +0900

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        {/* 여러 포맷 지원하려면 아래처럼 추가도 가능 */}
+        {/* <link rel="icon" type="image/png" href="/favicon.png" /> */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
